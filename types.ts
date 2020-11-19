fe1e14ec3ce8cc53dab8ef88ee579465d5e07160
Yasmin Hillis <jasminehillis94@gmail.com>
2020-11-19 21:31:07 +0200

    typescript

---
+++ types.ts
@@ -0,0 +1,33 @@
+console.log('Hello from ts');
+
+let myString: string; 
+let myNum: number; 
+let myBool:boolean
+let myVar:any
+
+// let strArr:string[]
+// let numArr: number[]
+// let boolArr:boolean[]
+
+let strArr: Array<string>
+let numArr: Array<number>
+let boolArr: Array<boolean>
+
+let strNumTuple: [string, number]
+
+myString = "hello".slice(0,3)
+myNum = 0xf00d
+myBool = false
+myVar = true
+strArr = ["heelo", 'world']
+numArr= [4,4,4,4,4,4,4]
+boolArr = [true, true, true, true]
+
+let myVoid: void = undefined
+let myUndefined: undefined = undefined
+let myNull: null = undefined
+
+strNumTuple = ["hello", 4]
+
+// console.log(myVoid)
+
